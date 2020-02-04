a112fbb33f4c7d4d39410de3c0dd4221c43e11a8
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-02-04 15:35:03 +0800

    hello world done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const wdio = require('webdriverio')
+const assert = require("assert")
+
+const opts = {
+  port: 4723,
+  capabilities: {
+    platformName: "Android",
+    platformVersion: "7",
+    deviceName: "ASUS_X018D",
+    app: "/home/carlomigueldy/fligno/helloworld/helloworld.apk",
+    appPackage: "io.appium.android.apis",
+    appActivity: ".view.TextFields",
+    automationName: "UiAutomator2"
+  }
+};
+
+async function main () {
+  const client = await wdio.remote(opts);
+
+  const field = await client.$("android.widget.EditText");
+  await field.setValue("Hello World!");
+  const value = await field.getText();
+  assert.equal(value,"Hello World!");
+
+  await client.deleteSession();
+}
+
+main();
+
